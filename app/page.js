bd831a6bb5fb743971e719b1ea305549610381b6
Sk Zubair Ahmed <sheikhzubairahmed3949@gmail.com>
2026-02-24 04:26:51 +0530

    Updated page.js

--- app/page.js
+++ app/page.js
@@ -3,8 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="container-fluid p-2 bg-white">
+      <div className="container-fluid p-2 bg-white flex flex-row gap-3">
         <h1 className="text text-black">SockText</h1>
+        <h1 className="text text-black">Home</h1>
+        <h1 className="text text-black">Rooms</h1>
       </div>
     </>
   );
