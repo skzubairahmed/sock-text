7265150f8fb67447b8e40cc72901082bc4dcddf1
Sk Zubair Ahmed <sheikhzubairahmed3949@gmail.com>
2026-02-24 04:18:19 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="container-fluid p-2 bg-white">
+        <h1 className="text text-black">SockText</h1>
+      </div>
+    </>
+  );
+}
